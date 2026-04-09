f4ad5eb554887c1c75ed0e8650fb550b3c2f05b4
CamielSamuels <56447369+SamuelsCamiel@users.noreply.github.com>
2026-04-09 21:04:44 +0200

    Javascript added

---
+++ Javascript/main.js
@@ -0,0 +1,16 @@
+function header() {
+document.querySelector("header").classList.toggle("active");
+document.getElementById("button").classList.toggle("active")
+}
+
+function linkedIn() {
+window.open("https://www.linkedin.com/in/camiel-samuels-1853392a3/")
+}
+
+function Github() {
+window.open("https://github.com/SamuelsCamiel")
+}
+
+function Email() {
+window.location.href = "mailto:samuelscamiel@gmail.com";
+}
