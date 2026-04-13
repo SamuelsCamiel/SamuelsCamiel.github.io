d544c2966c8bbffbfea34587ff3f41a18bf41b94
CamielSamuels <leimacs@gmail.com>
2026-04-13 14:38:33 +0200

    css change

---
+++ Javascript/about.js
@@ -0,0 +1,26 @@
+
+
+const reorder = () => {
+let windowWidth = window.innerWidth;
+
+  console.log("test")
+
+  const frag = document.createDocumentFragment();
+  const list = document.querySelector("container");
+
+  const items = list.querySelector("box");
+  const sortedList = [...items].sort((a, b, c) => {
+    const d = a.textContent,
+      e = b.textContent;
+      f = c.textContent;
+    return c < d ? -1 : c > d ? 1 : 0;
+  });
+  for (const item of sortedList) {
+    frag.appendChild(item);
+  }
+  list.appendChild(frag);
+
+    
+}
+reorder()
+
--- Javascript/main.js
+++ Javascript/main.js
@@ -3,8 +3,10 @@ document.querySelector("header").classList.toggle("active");
 document.getElementById("button").classList.toggle("active")
 }
 
+
+
 function burger() {
-    let windowWidth = window.innerWidth;
+let windowWidth = window.innerWidth;
     if(windowWidth < 951) {
         console.log(windowWidth)
         document.getElementById("header").classList.toggle("active")
@@ -13,6 +15,7 @@ function burger() {
         console.log(windowWidth)
     }
 }
+
 
 
 
